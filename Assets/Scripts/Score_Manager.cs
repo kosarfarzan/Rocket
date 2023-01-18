5e6bc6c766ae1d1c16c695551766a9c35c78cbaa
kosar <kosarfarzan7@gmail.com>
2023-01-19 02:31:08 +0330

    create score label

--- Assets/Scripts/Score_Manager.cs
+++ Assets/Scripts/Score_Manager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int score = 0;
+    public Text scoreText;
+
+
     void Start()
     {
+        StartCoroutine(Score());
+
+    }
+
+    
+    void Update()
+    {
+       scoreText.text = score.ToString();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator Score() {
+        while(true){
+        yield return new WaitForSeconds(2);
+        score = score + 1;
+        Debug.Log("Score:" + score);
+         }
     }
 }
